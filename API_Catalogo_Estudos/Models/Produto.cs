dd681890bdacdda1e73b48db1d46f290c724b50c
Everton Solon Tiba <evertonsolon@outlook.com>
2022-08-01 16:51:59 -0300

    Criação das entidades.

---
+++ API_Catalogo_Estudos/Models/Produto.cs
@@ -0,0 +1,12 @@
+﻿namespace API_Catalogo_Estudos.Models;
+
+public class Produto
+{
+    public int Id { get; set; }
+    public string? Nome { get; set; }
+    public decimal Preco { get; set; }
+    public string? ImagemUrl { get; set; }
+    public float Estoque { get; set; }
+    public DateTime DataCadastro { get; set; }
+
+}
