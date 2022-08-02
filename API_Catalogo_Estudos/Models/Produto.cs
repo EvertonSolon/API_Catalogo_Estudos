5593cfda3c7b981d79b9d4204beea98702642dd2
Everton Solon Tiba <evertonsolon@outlook.com>
2022-08-01 23:03:26 -0300

    Utilização de DataAnnotations nas Models

--- API_Catalogo_Estudos/Models/Produto.cs
+++ API_Catalogo_Estudos/Models/Produto.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations.Schema;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace API_Catalogo_Estudos.Models;
 
@@ -9,11 +10,19 @@ public class Produto
         Categoria = new Categoria();
     }
 
+    [Key]
     public int Id { get; set; }
+
+    [Required]
+    [MaxLength(80)]
     public string? Nome { get; set; }
-    
+
+    [Required]
     [Column(TypeName = "decimal(10,2)")]
     public decimal Preco { get; set; }
+
+    [Required]
+    [MaxLength(30)]
     public string? ImagemUrl { get; set; }
     public float Estoque { get; set; }
     public DateTime DataCadastro { get; set; }
